8191e5c740ed369cd7abbda90fd2f76c6e3b6451
Yuma Matsuni <yuma@yuma-macbookair.local>
2026-08-27 23:04:45 +0900

    submit week4

---
+++ week4/submissions/Yu-Ma28051503/sumcheck-is-all-you-need/python/solution.py
@@ -0,0 +1,269 @@
+import itertools
+import random
+
+
+class Polynomial:
+    """Class representing a multivariate polynomial.
+
+    terms: terms of a polynomial
+    p: order of finite field (a prime number)
+    
+    e.g.)
+        x*y + x + 2 = {
+            (1,1): 1,
+            (1,0): 1,
+            (0,0): 2
+        }
+    """
+    def __init__(
+        self,
+        terms: dict[tuple[int, ...], int],
+        p: int
+    ) -> None:
+        """Initializes `Polynomial` instance.
+
+        Args:
+            terms: terms of a polynomial
+            p: order of finite field (a prime number)
+        
+        Returns:
+            None
+        """
+        self.terms = terms
+        self.p = p
+
+    def reduce(self, x: int) -> int:
+        """Calculates x (mod p).
+        
+        Args:
+            x: target number
+        
+        Returns:
+            x (mod p)
+        """
+        return x % self.p
+
+    def evaluate(self, point: tuple[int, ...]) -> int:
+        """Evaluates polynomial at `point`.
+        
+        Args:
+            point: evaluation point
+        
+        Returns:
+            Evaluation result
+        """
+        result = 0
+        for t, a in self.terms.items():
+            mul = a
+            for i in range(len(point)):
+                mul *= point[i]**t[i]
+
+            result += self.reduce(mul)
+
+        return self.reduce(result)
+
+class UnivariatePolynomial(Polynomial):
+    """Class representing a univariate polynomial.
+
+    terms: coefficients of a univariate polynomial
+    p: order of finite field (a prime number)
+
+    e.g.)
+        [c_0, c_, ..., c_d] = c_0 + c_*x + ... + c_d*x^d
+    """
+    def __init__(
+        self,
+        coefficients: list[int],
+        p: int
+    ) -> None:
+        """Initializes `UnivariatePolynomial` instance.
+
+        Args:
+            coefficients: coefficients of a univariate polynomial
+            p: order of finite field (a prime number)
+        
+        Returns:
+            None
+        """
+        self.coefficients = coefficients
+        self.p = p
+
+    def evaluate(self, x: int) -> int:
+        """Evaluates polynomial at `x`.
+
+        Args:
+            x: evaluation point
+        
+        Returns:
+            Evaluation result
+        """
+        result = 0
+        for i in range(len(self.coefficients)):
+            result += (self.coefficients[i] * x**i) % self.p
+
+        return result % self.p
+
+class SumCheck:
+    """Class representing SumCheck protocol.
+
+    f: target function (i.e., polynomial or computation)
+    p: order of finite field of `f`
+    n: number of variables of `f`
+    """
+
+    def __init__(
+        self,
+        polynomial: Polynomial
+    ) -> None:
+        """Initializes `SumCheck` instance.
+
+        Args:
+            polynomial: target polynomial
+        
+        Returns:
+            None
+        """
+        self.f = polynomial
+        self.p = polynomial.p
+        self.n = len(next(iter(polynomial.terms)))
+
+    def gen_boolean_points(self, n: int) -> list[tuple[int, ...]]:
+        """Generates all the combinations of {0,1}^n.
+        
+        Args:
+            n: bit length
+        
+        Returns:
+            A set of all the vertices of n-dimensional boolean hypercube
+        """
+        return itertools.product([0, 1], repeat=n)
+
+    def calc_total_sum(self) -> int:
+        """Calculates Σf(x), ∀x ∈ {0,1}^n.
+        
+        Args:
+            None
+        
+        Returns:
+            Total sum
+        """
+        return self.f.reduce(
+            sum(self.f.evaluate(point) for point in self.gen_boolean_points(self.n))
+        )
+
+    def construct_round_polynomial(self, challenges: list[int]) -> UnivariatePolynomial:
+        """Constructs the round polynomial g_i(t) = Σf(r_1, ..., r_{i-1}, t, x_{i+1}, ..., x_n).
+
+        Args:
+            challenges: fixed random challenge values
+        
+        Returns:
+            Univariate polynomial for a single round
+        """
+        r = len(challenges)
+        num_free = self.n - r - 1
+
+        max_degree = max(term[r] for term in self.f.terms)
+        coeffs = [0] * (max_degree + 1)
+
+        for y_tuple in self.gen_boolean_points(num_free):
+            for term, coeff in self.f.terms.items():
+                value = coeff
+
+                for j in range(r):
+                    value *= challenges[j]**term[j]
+
+                for k in range(num_free):
+                    value *= y_tuple[k]**term[r + 1 + k]
+
+                t_degree = term[r]
+                coeffs[t_degree] += value
+
+        coeffs = [c % self.p for c in coeffs]
+
+        return UnivariatePolynomial(coeffs, self.p)
+
+    def prove(self, challenges:list[int] | None = None) -> list[tuple[UnivariatePolynomial, int]]:
+        """Generates a proof.
+
+        Args:
+            challenges: random challenge values
+        
+        Returns:
+            Proof (a list of tuples of round polynomial g_i(t) and random challenge r_i)
+        """
+        proof = []
+        fixed = []
+
+        for n in range(self.n):
+            g_i = self.construct_round_polynomial(fixed)
+
+            if challenges == None:
+                r = random.randint(0, self.p - 1)
+            else:
+                r = challenges[n]
+
+            proof.append((g_i, r))
+            fixed.append(r)
+
+        return proof
+
+    def verify(
+        self,
+        claimed_sum: int,
+        proof: list[tuple[UnivariatePolynomial, int]]
+    ) -> bool:
+        """Verifies a proof.
+
+        Args:
+            claimed_sum: Claimed sum of target function
+            proof: proof of the claimed sum
+        
+        Returns:
+            True if succeeded, false otherwise
+        """
+        fixed = []
+        g = None
+        r = None
+
+        for i, (g_i, r_i) in enumerate(proof):
+            check_value = (g_i.evaluate(0) + g_i.evaluate(1)) % self.p
+
+            if i == 0:
+                target = claimed_sum % self.p
+            else:
+                target = g.evaluate(r) % self.p
+
+            if check_value != target:
+                return False
+
+            fixed.append(r_i)
+            g, r = g_i, r_i
+
+        final_check = g.evaluate(r) % self.p
+        expected = self.f.evaluate(tuple(fixed)) % self.p
+
+        return final_check == expected
+
+if __name__ == "__main__":
+    # f = x*y + x + 2
+    polynomial = Polynomial(
+        terms={
+            (1, 1): 1,
+            (1, 0): 1,
+            (0, 0): 2
+        },
+        p=17
+    )
+
+    sc = SumCheck(polynomial=polynomial)
+
+    # f([0, 0]) + f([0, 1]) + f([1, 0]) + f([1, 0])
+    claimed_sum = sc.calc_total_sum()
+
+    proof = sc.prove(
+        challenges=[3, 5]
+    )
+
+    print("Claimed sum:", claimed_sum)
+    print("Proof verified?:", sc.verify(claimed_sum, proof))
